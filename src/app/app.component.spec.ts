03fb254a0d367645d942251009973c6ecc9bbcef
Guilherme <guima_dev@hotmail.com>
2020-09-15 01:05:46 -0300

    Criação do projeto de conversor de moedas. Adicionado bootstrap5, jquery e RxJS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'conversor-de-moedas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('conversor-de-moedas');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('conversor-de-moedas app is running!');
+    expect(app.title).toEqual('Conversor de Moedas');
   });
 });
